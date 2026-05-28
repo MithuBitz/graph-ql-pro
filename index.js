e9626eb474858661c4bc1d16e8514f3ab819bb6c
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2026-05-29 00:46:19 +0530

    Implement graphql as a proxy server

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import axios from "axios";
 
 const typeDefs = `#graphql
+
+    type Author {
+        name: String,
+        email: String,
+    }
+
     type Book {
         title: String
         subtitle: String
         publisher: String
         description: String
+        author: Author
         }
     type Query {
         books: [Book]
+        authors: [Author]
     }
 `;
 
 const resolvers = {
   Query: {
-    books: () => {
-      return [
-        {
-          title: "The Awakening",
-          subtitle: "A Novel",
-          publisher: "Harper & Brothers",
-          description: "The story of India",
+    books: async () => {
+      const { data } = await axios.get(
+        "https://api.freeapi.app/api/v1/public/books",
+      );
+
+      return data.data.data.map((book) => ({
+        title: book.volumeInfo.title,
+        subtitle: book.volumeInfo.subtitle,
+        publisher: book.volumeInfo.subtitle,
+        description: book.volumeInfo.description,
+        author: {
+          name: book.volumeInfo.authors[0],
         },
-      ];
+      }));
+
+      //   return [
+      //     {
+      //       title: "The Awakening",
+      //       subtitle: "A Novel",
+      //       publisher: "Harper & Brothers",
+      //       description: "The story of India",
+      //     },
+      //   ];
     },
   },
 };
